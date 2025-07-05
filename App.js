60a965607348d49aefab31b9ef712746ffde88fa
pankaj raut <167595891+pankajraut2186@users.noreply.github.com>
2025-07-05 19:50:21 +0530

    new changes

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PromptGenerator from './PromptGenerator';
+
+function App() {
+  return (
+    <div className="App">
+      <PromptGenerator />
+    </div>
+  );
+}
+
+export default App;
