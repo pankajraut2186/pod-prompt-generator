f6a47a6c81adad50a23d45dabdbe8543d0245a4a
pankaj raut <167595891+pankajraut2186@users.noreply.github.com>
2025-07-06 18:07:56 +0530

    resolving error

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import PromptGenerator from './PromptGenerator';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <PromptGenerator />
-  </React.StrictMode>
-);
+root.render(<PromptGenerator />);
