0973a28ca1ecc91de7ede935de3a5dce5f144d68
pankaj raut <167595891+pankajraut2186@users.noreply.github.com>
2025-07-06 19:14:42 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 import PromptGenerator from './PromptGenerator';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<PromptGenerator />);
+root.render(
+  <React.StrictMode>
+    <PromptGenerator />
+  </React.StrictMode>
+);
